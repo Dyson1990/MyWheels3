b822a6eca621bd5e196f0cf2f3ea3262d6538f62
Dyson <Weaver1990@163.com>
2020-06-28 12:09:39 +0800

    save

--- shell/add_admin.sh
+++ shell/add_admin.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 # create user script, need root.
 
+script_name=".sh"
+script_usage=$(cat <<EOF
+$script_name [USER NAME]
+EOF
+)
+script_function=$(cat <<EOF
+This script is used to add the current or specified users as system administrator.
+EOF
+)
+script_doc=$(cat <<EOF
+-h     Display this help.
+EOF
+)
+script_examples=$(cat <<EOF
+EOF
+)
+state_prefix="==="
+warning_prefix="***"
+error_prefix="!!!"
+
