42fbf703288428b0e4b4ebc80c00e64d7c1278ac
lyb <lyb@qiyandata.com>
2020-10-28 23:05:33 +0800

    'test'

--- hadoop/map_reduce/mr_test.py
+++ hadoop/map_reduce/mr_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 21 22:54:39 2020
@@ -6,3 +7,52 @@ Created on Wed Oct 21 22:54:39 2020
 """
 
 
+# # -*- coding:utf-8 -*-
+from mrjob.job import MRJob,MRStep
+
+class Top3_Mean_Friends(MRJob):
+    
+    def mapper0(self, _,line):
+        yield (None, line)
+    
+    def reducer0(self, _,line):
+        yield (None, '\n'.join(line))
+    
+    def mapper1(self, _,line):
+        '''
+        line:一行数据
+        (a,1)(b,1)(c,1)
+        (a,1)(c1)
+        (a1)
+       '''
+        pattern=re.compile(r'(\W+)')
+        for word in re.split(pattern=pattern,string=line):
+            if word.isalpha():
+                yield (word.lower(),1)
+
+
+    def reducer1(self,age,friends):
+        #shuff and sort 之后
+        '''
+        (a,[1,1,1])
+        (b,[1])
+        (c,[1])
+        '''
+        l=list(count)
+        yield (word,sum(l))
+        
+        
+    def mapper2(self, age,average_coun):
+        pass
+
+    def reducer2(self, _,average_list):        
+        pass
+
+    def steps(self):                 
+        return [
+            MRStep(mapper=self.mapper1,reducer=self.reducer1),
+            #MRStep(mapper=self.mapper2,reducer=self.reducer2)
+        ]
+
+if __name__ == '__main__':
+    Top3_Mean_Friends.run()
--- hadoop/map_reduce/mr_top3.py
+++ hadoop/map_reduce/mr_top3.py
@@ -1,5 +1,5 @@
-# #!/usr/bin/python
-# # -*- coding:utf-8 -*-
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 from mrjob.job import MRJob,MRStep
 
 class Top3_Mean_Friends(MRJob):
