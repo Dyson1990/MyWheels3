1dd33b42042c6356a2be11ff8209b11f3977bba1
lyb <lyb@qiyandata.com>
2020-10-29 00:06:05 +0800

    'test'

--- hadoop/map_reduce/mr_test.py
+++ hadoop/map_reduce/mr_test.py
@@ -16,32 +16,32 @@ import re
 
 WORD_RE = re.compile(r"[\w']+")
 
-class MRMostUsedWord(MRJob):
+class MRtest(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_words,
-                   combiner=self.combiner_count_words,
-                   reducer=self.reducer_count_words),
-            MRStep(reducer=self.reducer_find_max_word)
+            MRStep(mapper=self.mapper1,
+                   combiner=self.combiner1,
+                   reducer=self.reducer1),
+            MRStep(reducer=self.reducer2)
         ]
 
-    def mapper_get_words(self, _, line):
+    def mapper1(self, _, line):
         # yield each word in the line
         for word in WORD_RE.findall(line):
             yield (word.lower(), 1)
 
-    def combiner_count_words(self, word, counts):
+    def combiner1(self, word, counts):
         # optimization: sum the words we've seen so far
         yield (word, sum(counts))
 
-    def reducer_count_words(self, word, counts):
+    def reducer1(self, word, counts):
         # send all (num_occurrences, word) pairs to the same reducer.
         # num_occurrences is so we can easily use Python's max() function.
         yield None, (sum(counts), word)
 
     # discard the key; it is just None
-    def reducer_find_max_word(self, _, word_count_pairs):
+    def reducer2(self, _, word_count_pairs):
         # each item of word_count_pairs is (count, word),
         # so yielding one results in key=counts, value=word
         yield max(word_count_pairs)
@@ -55,18 +55,10 @@ class MRMostUsedWord(MRJob):
 
 if __name__ == '__main__':
     try:
-        MRMostUsedWord.run()
+        MRtest.run()
     except:
         Path(Path(__file__).parent, 'mr_test.log')\
             .write_text(traceback.format_exc())
-
-
-
-
-
-
-
-
 
 
 # =============================================================================
