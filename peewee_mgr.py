320c1287d8a1126b58858b4e4461d507defc8133
Dyson1990 <Dyson1990@163.com>
2023-06-27 16:19:48 +0800

    已经能流畅运行，并pandas.read_sql测试成功。

--- peewee_mgr.py
+++ peewee_mgr.py
@@ -6,15 +6,22 @@ Created on Mon Jun 19 15:53:26 2023
 """
 
 import peewee
+import importlib.util
+import inspect
+import re
 
 from loguru import logger
 from pathlib import Path
+py_dir = Path(__file__).parent
 
 try:
-    from . import db_cfg
+    # 从同级文件夹里读取db_cfg.py
+    spec = importlib.util.spec_from_file_location("db_cfg", py_dir/"db_cfg.py")
+    db_cfg = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(db_cfg)
 except Exception as e0:
+    # 从github读取db_cfg.py
     import requests
-    import importlib.util
     import types
     import sys
     owner = "Dyson1990"
@@ -43,43 +50,66 @@ databases = {
 
 class CustomDB(peewee.Database):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)  # 继承父类 Database 的初始化方法
-        
-        global databases
-        self.__class__ = type('CustomDB', (databases[kwargs['dialect']], CustomDB), {})
+        dialect = kwargs.get('dialect')
+        if dialect and dialect in databases:
+            database_class = databases[dialect]
+            super().__init__(**kwargs)
+            self.__class__ = type('CustomDB', (database_class, CustomDB), {})
+        else:
+            raise Exception("Unsupported database dialect")
+            
+        self.args = kwargs.copy()
+
+    def self_inspection(self):
+        """
+        去除会引起连接数据库失败的参数
+        """
+        while True:
+            try:
+                # 尝试创建实例并传递关键字参数
+                super().__init__(**self.args)
+                with self:
+                    pass
+                break
+            except TypeError as e:
+                # 提取异常中的无效参数信息
+                error_message = str(e)
+                re_str = r"Connection\.__init__\(\) got an unexpected keyword argument\s*'(\w+)'"
+                m = re.search(re_str, error_message)
+                if m:
+                    arg0 = m.groups()[0]
+                    logger.warning(f"检查到无效参数，已剔除：{arg0}")
+                    self.args.pop(arg0)
+                else:
+                    raise e
+
+# =============================================================================
+#         super().__init__(**kwargs)  # 继承父类 Database 的初始化方法
+# 
+#         global databases
+#         print(kwargs['dialect'])
+#         self.__class__ = type('CustomDB', (databases[kwargs['dialect']], CustomDB), {})
+# =============================================================================
 
 
 if __name__ == '__main__':
+    import pandas as pd
     sql_args = {
         'db_dialect': 'MySQL'
         , 'db_driver': 'pymysql'
-        , "host": "192.168.1.125"
+        , "host": "192.168.1.190"
         , 'username': "Dyson"
         , "password": "1qqaq1"
         , 'dbname': 'test'
-        , 'table_name': 'vote_record'
     }
     cfg = db_cfg.DBConfig('MySQL', **sql_args)
     cfg.fit_peewee()
     
     db = CustomDB(**cfg)
-    print(db)
+    db.self_inspection()
+    
+    # 运行原始 SQL 语句
+    query = "SELECT * FROM employees_test LIMIT 10"
+    df = pd.read_sql(query, db)
+    print(df)
 
-# # 定义 MySQL 数据库连接
-# # db = MySQLDatabase('mydatabase', user='myuser', password='mypassword',
-# #                    host='myhost', port=3306)
-
-# # 执行原生 SQL 查询
-# cursor = db.execute_sql("SELECT * FROM mytable")
-
-# # 一行一行读取数据并处理
-# while True:
-#     batch = cursor.fetchmany(1000)
-#     if not batch:
-#         break
-#     for row in batch:
-#         # 处理每一行数据
-#         print(row)
-
-# # 关闭游标
-# cursor.close()
