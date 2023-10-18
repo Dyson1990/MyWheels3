8bf5bb539f193da38e87f4a6da44fbf78175c92f
Dyson <Dyson1990@163.com>
2023-10-18 17:17:32 +0800

    更新散装函数为类

--- remote_pycode.py
+++ remote_pycode.py
@@ -11,43 +11,46 @@ import importlib.util
 import types
 import sys
 
-def from_github(branch, module_p):
-    owner = "Dyson1990"
-    repo = "MyWheels3"
-    # 从GitHub API获取源代码并打包成字节码
-    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{module_p}'
-    params = {'ref': branch} if branch else None
-    headers = {'Accept': 'application/vnd.github.v3.raw'}
-    response = requests.get(url, params=params, headers=headers)
-    source_code_raw = response.text
+class RemotePyCode():
+    def __init__(self, source="github", owner="Dyson1990", repo="MyWheels3", branch=None):
+        self.owner=owner
+        self.repo=repo
+        self.source=source
+        self.branch=branch
+        
+        if self.source.lower() == "github": # 从GitHub API获取源代码并打包成字节码
+            self.api_url = f'https://api.github.com/repos/{self.owner}/{self.repo}/contents/{{module_p}}'
+            self.params = {'ref': self.branch} if self.branch else None
+            self.headers = {'Accept': 'application/vnd.github.v3.raw'}
+        else:
+            raise Exception(f"unknown source: {self.source}")
     
-    return source_code_raw
-
-def create_module(long_text, module_name):
-    # 创建规范
-    spec = importlib.util.spec_from_loader(module_name, loader=None, origin="<string>")
-    
-    # 创建新模块对象
-    new_module = types.ModuleType(spec.name)
-    
-    # 在新模块中执行 Python 代码
-    exec(long_text, new_module.__dict__)
-    
-    # 将新模块添加到 sys.modules 字典中和全局命名空间中
-    sys.modules[spec.name] = new_module
-    globals()[spec.name] = new_module
-    
-    # 返回新创建的模块对象
-    return new_module
+    def create_module(self, module_p, module_name=''):
+        url = self.api_url.format(module_p=module_p)
+        response = requests.get(url
+                                , params=self.params
+                                , headers=self.headers
+                               )
+        self.pycode_raw = response.text
+        
+        # 创建规范
+        spec = importlib.util.spec_from_loader(module_name, loader=None, origin="<string>")
+        
+        # 创建新模块对象
+        new_module = types.ModuleType(spec.name)
+        
+        # 在新模块中执行 Python 代码
+        exec(self.pycode_raw, new_module.__dict__)
+        
+        # 将新模块添加到 sys.modules 字典中和全局命名空间中
+        sys.modules[spec.name] = new_module
+        globals()[spec.name] = new_module
+        
+        # 返回新创建的模块对象
+        return new_module
 
 if __name__ == "__main__":
-    branch = "master"
-    module_p = "sql_manager.py"
+    github_con = RemotePyCode()
+    requests_mgr = github_con.create_module("requests_mgr.py")
 
-    # 从Github中读取mysql_manager.py文件，得到my_module对象
-    source_code_raw = from_github(branch, module_p)
-    module_name = "my_module"
-    new_module = create_module(source_code_raw, module_name)
-    
-    # 调用新模块的 hello() 函数
-    print(new_module.check_json)
+    print(dir(requests_mgr))
